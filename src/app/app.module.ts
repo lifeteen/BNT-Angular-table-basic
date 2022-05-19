0c23a7814a2dee3b6c5ff4e0e91caa18fbcdbca4
unknown <lifeteen1999@gmail.com>
2022-05-19 09:14:28 +0700

    Angular table bassic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgbdTableBasic } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, CommonModule, NgbModule],
+  declarations: [NgbdTableBasic],
+  exports: [NgbdTableBasic],
+  bootstrap: [NgbdTableBasic]
 })
-export class AppModule { }
+export class NgbdTableBasicModule {}
